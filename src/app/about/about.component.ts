31b0b44240adb2167e881429e75b8905e2d153ca
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-03-21 20:41:20 +0530

    changes don

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
  skillArray:any= ["Angular", "Angular Material", "JSON", "Axios", "TypeScript", "AngularJS", "GitHub", "Microsoft Visual Studio Code", "IntelliJ IDEA", "MySQL", "Databse", "Java", "Core Java", "JavaScript", "Java Database Connectivity (JDBC)", "HTML", "Cascading Style Sheets (CSS)"]
+
+
+ getAge() {
+  const birthDate = new Date('1998-06-23');
+  const currentDate = new Date();
+  const diffInMs = currentDate.getTime() - birthDate.getTime();
+  const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
+  const age = Math.round(diffInYears);
+  return age;
+}
+
 }
