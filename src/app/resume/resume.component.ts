5aa281240aab7e8f6bd42fcc18fbf2d0516bd036
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-04-08 19:22:20 +0530

    done

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { myEdu } from '../myEducation';
 import { myJob } from '../myJobs';
+import {saveAs} from 'file-saver';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
@@ -16,5 +17,8 @@ fetchData:any="";
 
 imageSend(data: any) {
   this.fetchData =data;
+}
+downLoad(){
+  saveAs('../../assets/Priyanshu Negi.pdf', 'Download Resume')
 }
 }
